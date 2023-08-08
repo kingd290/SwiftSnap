f59f06801f7d16db4c3c02525d7fb0248ff5f340
kd9s0 <zetazeros@protonmail.com>
2023-08-08 12:26:32 -0700

    update log_file

--- src/create_snapshot.sh
+++ src/create_snapshot.sh
@@ -3,4 +3,4 @@
 # AWS CLI Command to Create EBS Snapshot
 volume_id="your_ebs_volume_id_here"
 snapshot_description="Snapshot taken on $(date +'%Y-%m-%d %H:%M:%S')"
-aws ec2 create-snapshot --volume-id "$volume_id" --description "$snapshot_description"
+log_file="snapshot_log.txt"
