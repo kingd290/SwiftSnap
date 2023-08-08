deb4a095eca6105a8633678250a2a6e35b20fe87
kd9s0 <zetazeros@protonmail.com>
2023-08-08 12:27:24 -0700

    function to create snapshot

--- src/create_snapshot.sh
+++ src/create_snapshot.sh
@@ -4,3 +4,12 @@
 volume_id="your_ebs_volume_id_here"
 snapshot_description="Snapshot taken on $(date +'%Y-%m-%d %H:%M:%S')"
 log_file="snapshot_log.txt"
+
+# Function to create EBS snapshot
+create_ebs_snapshot() {
+    local volume_id="$1"
+    local snapshot_description="$2"
+    
+    echo "Creating EBS snapshot for volume $volume_id..."
+    aws ec2 create-snapshot --volume-id "$volume_id" --description "$snapshot_description"
+}
