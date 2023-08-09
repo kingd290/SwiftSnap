3cf0ad708677afe25292935d39cc411e54c23c50
kd9s0 <zetazeros@protonmail.com>
2023-08-09 13:05:08 -0700

    modified Log event function

--- src/create_snapshot.sh
+++ src/create_snapshot.sh
@@ -1,58 +1,50 @@
 #!/bin/bash
 
-# AWS CLI Command to Create EBS Snapshot
-volume_id="your_ebs_volume_id_here"
+volume_id="your_ebs_volume_id"
 snapshot_description="Snapshot taken on $(date +'%Y-%m-%d %H:%M:%S')"
 log_file="snapshot_log.txt"
+notification_email="zetazeros@proton.me"
+aws_region="us-east-1"
 
-# function to create EBS snapshot
 create_ebs_snapshot() {
     local volume_id="$1"
     local snapshot_description="$2"
     
     echo "Creating EBS snapshot for volume $volume_id..."
-    aws ec2 create-snapshot --volume-id "$volume_id" --description "$snapshot_description"
+    aws ec2 create-snapshot --volume-id "$volume_id" --description "$snapshot_description" --region "$aws_region"
 }
 
-# function to clean up old snapshots (keep only the latest N snapshots)
-cleanup_snapshots() {
-    local volume_id="$1"
-    local num_snapshots_to_retain=5
-
-    echo "Cleaning up old snapshots for volume $volume_id..."
-    snapshot_ids=$(aws ec2 describe-snapshots --filters "Name=volume-id,Values=$volume_id" --query "Snapshots | sort_by(@, &StartTime) | [:-( $num_snapshots_to_retain )].SnapshotId" --output text)
-    if [[ -n "$snapshot_ids" ]]; then
-        for snapshot_id in $snapshot_ids; do
-            echo "Deleting snapshot: $snapshot_id"
-            aws ec2 delete-snapshot --snapshot-id "$snapshot_id"
-        done
-    else
-        echo "No old snapshots found."
-    fi
+tag_snapshot() {
+    local snapshot_id="$1"
+    
+    aws ec2 create-tags --resources "$snapshot_id" --tags "Key=Name,Value=EBS-Snapshot" "Key=CreatedBy,Value=EBS-Snapshot-Script" --region "$aws_region"
 }
 
-log_event() {
-    local event="$1"
-    echo "$(date +'%Y-%m-%d %H:%M:%S') - $event" >> "$log_file"
+send_notification() {
+    local subject="$1"
+    local message="$2"
+
+    echo "$message" | mail -s "$subject" "$notification_email"
 }
 
-# main execution script
-if [[ -z "$volume_id" || "$volume_id" == "your_ebs_volume_id_here" ]]; then
-    echo "ERROR: Please replace 'your_ebs_volume_id_here' with your actual EBS volume ID."
+# Main script execution
+if [[ -z "$volume_id" || "$volume_id" == "your_ebs_volume_id" ]]; then
+    echo "ERROR: Please replace 'your_ebs_volume_id' with your actual EBS volume ID."
     exit 1
 fi
 
-# check if AWS CLI is installed
+# Check if AWS CLI is installed
 if ! command -v aws &> /dev/null; then
     echo "ERROR: AWS CLI is not installed. Please install it and configure with appropriate IAM credentials."
     exit 1
 fi
 
-# create EBS snapshot
-create_ebs_snapshot "$volume_id" "$snapshot_description"
+snapshot_id=$(create_ebs_snapshot "$volume_id" "$snapshot_description")
 
-# clean old snapshots
-cleanup_snapshots "$volume_id"
+tag_snapshot "$snapshot_id"
 
-# log event
-log_event "EBS snapshot created and old snapshots cleaned up."
+echo "$(date +'%Y-%m-%d %H:%M:%S') - EBS snapshot created: $snapshot_id" >> "$log_file"
+
+notification_subject="EBS Snapshot Created"
+notification_message="An EBS snapshot has been created with ID: $snapshot_id"
+send_notification "$notification_subject" "$notification_message"
