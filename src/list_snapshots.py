49507f9e39aa4d0b0d7c873e8ace26951b128797
kd9s0 <zetazeros@protonmail.com>
2023-08-15 15:29:27 -0700

    snapshot log

--- src/list_snapshots.py
+++ src/list_snapshots.py
@@ -1,18 +1,14 @@
 import boto3
 
-# AWS Configuration
 aws_region = "us-east-1"
 
-# Initialize Boto3 Client
 ec2_client = boto3.client("ec2", region_name=aws_region)
 
 def list_snapshots():
     print("Listing EBS Snapshots:\n")
-
-    # Retrieve snapshots
+    
     response = ec2_client.describe_snapshots(OwnerIds=["self"])
 
-    # Display snapshot details
     for snapshot in response["Snapshots"]:
         snapshot_id = snapshot["SnapshotId"]
         volume_id = snapshot["VolumeId"]
@@ -26,7 +22,6 @@ def list_snapshots():
         print(f"State: {state}")
         print(f"Description: {description}")
         
-        # Retrieve tags for the snapshot
         tags_response = ec2_client.describe_tags(Filters=[{"Name": "resource-id", "Values": [snapshot_id]}])
         tags = tags_response.get("Tags", [])
         
